8c8cd1bab5c663f7f5c10ecb923286033e23ede7
trogucki <trogucki@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-01-28 12:50:21 +0000

    Continuous Integration enhancement 10
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/bindings/PyTango/trunk@21955 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- ci/ALBA/build.sh
+++ ci/ALBA/build.sh
@@ -8,7 +8,7 @@ export LD_LIBRARY_PATH=/tmp/jenkins/jobs/TangoLib/lib:$LD_LIBRARY_PATH
 export CPLUS_INCLUDE_PATH=/tmp/jenkins/jobs/TangoLib/include:$CPLUS_INCLUDE_PATH
 
 echo $LD_LIBRARY_PATH
-echo $LD_LIBRARY_PATH
+echo $CPLUS_INCLUDE_PATH
 
 cd ../..
 
@@ -19,6 +19,8 @@ make user=1 prefix=/tmp/jenkins/jobs install
 make user=1 prefix=/tmp/jenkins/jobs install
 
 export PYTHONPATH=/tmp/jenkins/jobs/PyTango:$PYTHONPATH
+
+echo $PYTHONPATH
 
 python tests/DevTest.py pytomasz &
 
