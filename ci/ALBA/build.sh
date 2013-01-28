bd9890e1eb16d87aefa2308efa47ab5aca1a5de6
trogucki <trogucki@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-01-28 11:45:56 +0000

    Continuous Integration enhancement 8
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/bindings/PyTango/trunk@21952 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

---
+++ ci/ALBA/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "In ALBA/build.sh"
+
+pwd
---
+++ ci/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pwd
+
+
+if [ ! -z "$INSTITUTE" -a -d "$INSTITUTE" -a -f "$INSTITUTE/build.sh" ]
+then
+	echo "Executing build.sh for $INSTITUTE"
+	cd "$INSTITUTE"
+	./build.sh
+	exit 0
+else
+	if [ ! -z "$INSTITUTE" ]
+	then
+		echo "Failed to execute ci/$INSTITUTE/build.sh!"
+		echo "Make sure ci/$INSTITUTE/build.sh exists"
+	else
+		echo "Mr Jenkins needs additional configuration!"
+		echo "Go to Jenkins dashboard -> Manage Jenkins -> Global Properties, tick Environment Variables and add a key-value pair: name - INSTITUTE, value - YourInstituteName."
+		echo "Check out the project. Go to the 'ci' directory and create a 'YourInstituteName' subdirectory. In the 'YourInstituteName' subdirectory create a 'build.sh' file which will contain the recipe how to build your project. Make the 'build.sh' file executable. Commit changes."
+	fi
+	exit -1
+fi
