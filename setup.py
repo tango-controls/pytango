41d701c464cd63619f4c8961f9cf90e7714d9beb
trogucki <trogucki@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-01-30 17:52:25 +0000

    Modified setup.py to take into account lib64 3
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/bindings/PyTango/trunk@22002 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- setup.py
+++ setup.py
@@ -379,8 +379,14 @@ def main():
     if not has_numpy():
         macros.append( ('DISABLE_PYTANGO_NUMPY', None) )
 
-    #library_dirs = [os.path.join(f,'lib' if os.stat(os.path.join(f,'lib')) else 'lib64') for f in (TANGO_ROOT,BOOST_ROOT,LOG4TANGO_ROOT,ZMQ_ROOT)]
-    library_dirs = [(d if os.stat(d) else d.replace('lib','lib64')) for d in (os.path.join(f,'lib') for f in (TANGO_ROOT,BOOST_ROOT,LOG4TANGO_ROOT,ZMQ_ROOT))]
+    library_dirs = []
+    for f in (TANGO_ROOT,BOOST_ROOT,LOG4TANGO_ROOT,ZMQ_ROOT):
+        d = os.path.join(f,'lib')
+        try: 
+            if not os.stat(d): raise Exception('%s_doesnt_exists'%d)
+        except: d.replace('lib','lib64')
+        library_dirs.append(d)
+            
     print('library_dirs: %s'%library_dirs)
 #    library_dirs = [
 #        os.path.join(TANGO_ROOT, 'lib'),
