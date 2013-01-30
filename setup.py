4765fc04d5249c9066f434e482bace2b335520fe
trogucki <trogucki@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-01-30 17:44:30 +0000

    Modified setup.py to take into account lib64 2
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/bindings/PyTango/trunk@22001 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- setup.py
+++ setup.py
@@ -381,6 +381,7 @@ def main():
 
     #library_dirs = [os.path.join(f,'lib' if os.stat(os.path.join(f,'lib')) else 'lib64') for f in (TANGO_ROOT,BOOST_ROOT,LOG4TANGO_ROOT,ZMQ_ROOT)]
     library_dirs = [(d if os.stat(d) else d.replace('lib','lib64')) for d in (os.path.join(f,'lib') for f in (TANGO_ROOT,BOOST_ROOT,LOG4TANGO_ROOT,ZMQ_ROOT))]
+    print('library_dirs: %s'%library_dirs)
 #    library_dirs = [
 #        os.path.join(TANGO_ROOT, 'lib'),
 #        os.path.join(BOOST_ROOT, 'lib'),
