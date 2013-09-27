06cd71468d08996b328f55926d84faa4cfbbde23
cmft <cmft@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-09-27 15:17:59 +0000

    Update ci/ESRF/build.sh
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/bindings/PyTango/trunk@23813 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- ci/ESRF/build.sh
+++ ci/ESRF/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+#export CYGWIN="${CYGWIN} nodosfilewarning"
 INSTALL_DIR=/segfs/tango/ci/PyTango
+export PATH=$PATH:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 9.0/Common7/IDE 
 
 if [ ! -z "$NODE_NAME" -a -f "$NODE_NAME" ]
 then
-	if [ $NODE_NAME = "ct32windows7" ]
+	print $NODE_NAME
+	if [ $NODE_NAME != "ct32windows7" ]
 	then
-        ./$NODE_NAME
-    else
         source "$NODE_NAME"
     fi
 else
@@ -20,17 +20,39 @@ cd ../..
 
 rm -rf build/*
 
+print $realos
 case "${realos}" in
-	"debian6_64")
+	"debian"*)
+		#debian6/7
         /usr/bin/python setup.py build
 		;; 
 	"redhate"*)
         #redhate4/5
         /segfs/bliss/bin/python2.6 setup.py build
         ;;
-    "windows7"*)
-        #VC9
-        /cygdrive/c/Python27_32/python setup.py build
+	"windows7")
+		pyVersion="py26"
+		platform="Win32"
+		SLN="C:\jenkins\workspace\PyTango\OperatingSystems\Windows64-VC10\win\PyTango_VS9\PyTango.sln"
+		LIBNAME="PyTango.vcproj"
+		#devenv="C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" 
+		cd /cygdrive/c
+		pyVersion="py26"
+		platform="Win32"
+		SLN="C:\jenkins\workspace\PyTango\OperatingSystems\Windows64-VC10\win\PyTango_VS9\PyTango.sln"
+		LIBNAME="PyTango.vcproj"
+		#devenv="\cygdrive\c\Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 9.0/Common7/IDE/devenv.exe"
+		for pv in $pyVersion
+		do
+		   for p in $platform
+		   do
+			   pyN=${pv:2:4}
+			   MODE=$pv"_bopyshared_tangoshared_release|"$p
+			   OUTFILE="c:/Temp/log_${p}_"${pyN}
+			   MAKE_CMD="devenv.exe $SLN /project PyTango.vcproj /rebuild $MODE /projectconfig $MODE /out $OUTFILE"
+			   $MAKE_CMD
+		   done
+		done
         ;;
 	*)
 		echo "Build - Not supporting operating system: " ${OSTYPE}
@@ -48,6 +70,10 @@ case "${realos}" in
         rm -rf $INSTALL_DIR/debian6/*
 		/usr/bin/python setup.py install --prefix=$INSTALL_DIR/debian6
 		;; 
+	"debian7_64")
+        rm -rf $INSTALL_DIR/debian6/*
+		/usr/bin/python setup.py install --prefix=$INSTALL_DIR/debian7
+		;; 
 	"redhate4_32")
         rm -rf $INSTALL_DIR/redhate4/*
 		/segfs/bliss/bin/python2.6 setup.py install --prefix=$INSTALL_DIR/redhate4
@@ -56,8 +82,9 @@ case "${realos}" in
         rm -rf $INSTALL_DIR/redhate5/*
 		/segfs/bliss/bin/python2.6 setup.py install --prefix=$INSTALL_DIR/redhate5
 		;;
-    "windows7_32"*)
-        /cygdrive/c/Python27_32/python setup.py install --prefix=$INSTALL_DIR/w7_32_VC9
+    "windows7"*)
+        cp -rf /cygdrive/c/Temp/pytango/build_8.1.0_tg8.1.2_boost1.53.0/lib  /cygdrive/s/tango/ci/PyTango/Windows7
+		cp -rf /cygdrive/c/Temp/pytango/build_8.1.0_tg8.1.2_boost1.53.0/dist  /cygdrive/s/tango/ci/PyTango/Windows7
         ;;
 	*)
 		echo "Install - Not supporting operating system: " ${OSTYPE}
@@ -67,5 +94,6 @@ esac
 
 
 exit $?
+
 
 
