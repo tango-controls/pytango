9aee730c624b4d32c46e2391aba85e7449328fee
tiagocoutinho <tiagocoutinho@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2014-04-02 14:32:07 +0000

    Implement feature-request #103
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/bindings/PyTango/trunk@25332 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- src/boost/cpp/event_data.cpp
+++ src/boost/cpp/event_data.cpp
@@ -12,13 +12,44 @@
 #include "precompiled_header.hpp"
 #include <tango.h>
 
+#include "exception.h"
+
 using namespace boost::python;
+
+extern boost::python::object PyTango_DevFailed;
+
+namespace PyEventData
+{
+    static boost::shared_ptr<Tango::EventData> makeEventData()
+    {
+        Tango::EventData *result = new Tango::EventData;
+        result->attr_value = new Tango::DeviceAttribute();
+       return boost::shared_ptr<Tango::EventData>(result);
+    }
+
+    static void set_errors(Tango::EventData &event_data, boost::python::object &error)
+    {
+        PyObject* error_ptr = error.ptr();
+        if (PyObject_IsInstance(error_ptr, PyTango_DevFailed.ptr()))
+        {
+            Tango::DevFailed df;
+	    boost::python::object error_list = error.attr("args");
+	    sequencePyDevError_2_DevErrorList(error_list.ptr(), event_data.errors);
+        }
+        else
+        {
+            sequencePyDevError_2_DevErrorList(error_ptr, event_data.errors);
+        }
+    }
+};
 
 void export_event_data()
 {
     class_<Tango::EventData>("EventData",
         init<const Tango::EventData &>())
     
+        .def("__init__", boost::python::make_constructor(PyEventData::makeEventData))
+
         // The original Tango::EventData structure has a 'device' field.
         // However, if we returned this directly we would get a different
         // python device each time. So we are doing our weird things to make
@@ -27,8 +58,8 @@ void export_event_data()
         // See callback.cpp
         .setattr("device", object())
         
-        .def_readonly("attr_name", &Tango::EventData::attr_name)
-        .def_readonly("event", &Tango::EventData::event)
+        .def_readwrite("attr_name", &Tango::EventData::attr_name)
+        .def_readwrite("event", &Tango::EventData::event)
         
         // The original Tango::EventData structure has "get_attr_value" but
         // we can't refer it directly here because we have to extract value
@@ -36,10 +67,12 @@ void export_event_data()
         // See callback.cpp
         .setattr("attr_value", object())
         
-        .def_readonly("err", &Tango::EventData::err)
-        .def_readonly("reception_date", &Tango::EventData::reception_date)
-        .add_property("errors", make_getter(&Tango::EventData::errors, 
-            return_value_policy<copy_non_const_reference>()))
+        .def_readwrite("err", &Tango::EventData::err)
+        .def_readwrite("reception_date", &Tango::EventData::reception_date)
+        .add_property("errors", 
+		      make_getter(&Tango::EventData::errors, 
+				  return_value_policy<copy_non_const_reference>()),
+		      &PyEventData::set_errors)
         
         .def("get_date", &Tango::EventData::get_date, 
             return_internal_reference<>())
