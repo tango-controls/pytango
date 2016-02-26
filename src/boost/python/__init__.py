5924960d37958bdaa6178ded6a6554aef18d83da
Jose Tiago Coutinho Macara <tiago.coutinho@esrf.fr>
2016-02-26 13:09:09 +0100

    Remove leavefunc code (not used)

--- src/boost/python/__init__.py
+++ src/boost/python/__init__.py
@@ -197,12 +197,3 @@ from .pytango_init import init as __init
 __init()
 
 
-def __leave():
-    try:
-        _PyTango._leavefunc()
-    except:
-        pass
-
-#import atexit
-#atexit.register(__leave)
-
