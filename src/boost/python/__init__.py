16d89740ca84eb56dae2a2ad5a1f4d617760ea2f
Jose Tiago Coutinho Macara <tiago.coutinho@esrf.fr>
2016-02-26 13:09:09 +0100

    Remove leavefunc code (not used)

--- src/boost/python/__init__.py
+++ src/boost/python/__init__.py
@@ -193,11 +193,3 @@ from .tango_numpy import NumpyType, numpy_type, numpy_spectrum, numpy_image
 
 from .pytango_init import init as __init
 __init()
-
-
-def __leave():
-    _PyTango._leavefunc()
-
-import atexit
-atexit.register(__leave)
-
