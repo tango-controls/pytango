7db0c4d5175f551398282819381be7759adfe90b
Vincent Michel <vincent.michel@maxlab.lu.se>
2017-03-01 18:11:52 +0100

    Update server tests
    - Use python types instead of tango types
    - Add spectrum types to typed_values fixture
    - Add tests for device properties without default value

--- tango/test_utils.py
+++ tango/test_utils.py
@@ -12,6 +12,11 @@ try:
 except ImportError:
     pytest = None
 
+try:
+    import numpy.testing
+except ImportError:
+    numpy = None
+
 __all__ = ['DeviceTestContext', 'SimpleDevice']
 
 
@@ -22,6 +27,36 @@ class SimpleDevice(Device):
         self.set_state(DevState.ON)
 
 
+# Helpers
+
+TYPED_VALUES = {
+    int: (1, 2),
+    float: (2.71, 3.14),
+    str: ('hey hey', 'my my'),
+    bool: (False, True),
+    (int,): ([1, 2, 3], [9, 8, 7]),
+    (float,): ([0.1, 0.2, 0.3], [0.9, 0.8, 0.7]),
+    (str,): (['ab', 'cd', 'ef'], ['gh', 'ij', 'kl']),
+    (bool,): ([False, False, True], [True, False, False])}
+
+
+def repr_type(x):
+    if not isinstance(x, tuple):
+        return x.__name__
+    return '({},)'.format(x[0].__name__)
+
+
+# Numpy helpers
+
+if numpy and pytest:
+
+    def assert_close(a, b):
+        try:
+            assert a == pytest.approx(b)
+        except ValueError:
+            numpy.testing.assert_allclose(a, b)
+
+
 # Pytest fixtures
 
 if pytest:
@@ -30,22 +65,11 @@ if pytest:
     def state(request):
         return request.param
 
-    @pytest.fixture(params=utils._scalar_types)
+    @pytest.fixture(
+        params=list(TYPED_VALUES.items()),
+        ids=lambda x: repr_type(x[0]))
     def typed_values(request):
-        dtype = request.param
-        # Unsupported types
-        if dtype in [CmdArgType.DevInt, CmdArgType.ConstDevString,
-                     CmdArgType.DevEncoded, CmdArgType.DevUChar]:
-            pytest.xfail('Should we support those types?')
-        # Supported types
-        if dtype in utils._scalar_str_types:
-            return dtype, ['hey hey', 'my my']
-        if dtype in utils._scalar_bool_types:
-            return dtype, [False, True]
-        if dtype in utils._scalar_int_types:
-            return dtype, [1, 2]
-        if dtype in utils._scalar_float_types:
-            return dtype, [2.71, 3.14]
+        return request.param
 
     @pytest.fixture(params=GreenMode.values.values())
     def green_mode(request):
