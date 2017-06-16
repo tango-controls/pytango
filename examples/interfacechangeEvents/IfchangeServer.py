a62929ba1321433503760b3ef80116add80ff081
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2017-06-16 10:22:33 +0100

    pep8 changes

--- examples/interfacechangeEvents/IfchangeServer.py
+++ examples/interfacechangeEvents/IfchangeServer.py
@@ -1,33 +1,20 @@
-
-# PyTango imports
-import PyTango
-from PyTango import DebugIt
-from PyTango.server import run
-from PyTango.server import Device, DeviceMeta
-from PyTango.server import attribute, command
-from PyTango.server import class_property, device_property
-from PyTango import AttrQuality, AttrWriteType, DispLevel, DevState
-
 import numpy
 import gevent
 import logging
-from dbus.server import Server
+
+import tango
+from tango import DevState
+from tango.server import run
+from tango.server import Device
+from tango.server import attribute, command
+
 
 class IfchangeServer(Device):
-
-    __metaclass__ = DeviceMeta
-
-    # ---------------
-    # General methods
-    # ---------------
-
-    def __init__(self, *args, **kwargs):
-        Device.__init__(self, *args, **kwargs)
 
     def init_device(self):
         Device.init_device(self)
         logging.basicConfig(level=logging.DEBUG)
-        self.set_state(PyTango.DevState.ON)
+        self.set_state(tango.DevState.ON)
 
     @attribute(label='Sequence Counter', dtype='int',
         description="Sequence counter")
@@ -44,13 +31,13 @@ class IfchangeServer(Device):
 
     def write_current(self, curr):
         self._current = curr
-    
+
     @command(dtype_in=str, doc_in='name of dynamic attribute to add')
     def add_dyn_attr(self, name):
         attr = attribute(name=name, dtype='float', 
-                          fget=self.read_current, fset=self.write_current)
+                         fget=self.read_current, fset=self.write_current)
         self.add_attribute(attr)
-        
+
     @command(dtype_in=str, doc_in='name of dynamic attribute to delete')
     def delete_dyn_attr(self, name):
         self._remove_attribute(name)
@@ -59,11 +46,5 @@ class IfchangeServer(Device):
 # Run server
 # ----------
 
-def main():
-    from PyTango import GreenMode
-    from PyTango.server import run
-
-    run([IfchangeServer,], green_mode=GreenMode.Gevent)
-
 if __name__ == '__main__':
-    main()
+    IfchangeServer.run_server()
