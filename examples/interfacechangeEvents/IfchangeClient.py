55b1b1eb4d15281a6ab7cc60e1397cc426bce08b
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2017-06-16 13:46:36 +0100

    python 3 print's

--- examples/interfacechangeEvents/IfchangeClient.py
+++ examples/interfacechangeEvents/IfchangeClient.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import time
 import numpy
 
@@ -10,7 +12,7 @@ class EventManager():
     def __init__(self, dp):
         self._deviceProxy = dp
         if dp is not None:
-            print "Subscribed to Interface Change Events"
+            print("Subscribed to Interface Change Events")
             self._event_id = dp.subscribe_event(
                 tango.EventType.INTERFACE_CHANGE_EVENT, self)
 
@@ -18,36 +20,36 @@ class EventManager():
         self._deviceProxy.unsubscribe_event(self._event_id)
 
     def push_event(self, ev):
-        print "Event -----push_event-----------------"
-        print "Timestamp:      ", ev.reception_date
-        print "Event type:     ", ev.event
-        print "Device server:  ", ev.device
-        print "Event error:    ", ev.err
+        print("Event -----push_event-----------------")
+        print("Timestamp:      ", ev.reception_date)
+        print("Event type:     ", ev.event)
+        print("Device server:  ", ev.device)
+        print("Event error:    ", ev.err)
         if ev.err:
-            print "Caught pipe exception"
+            print("Caught pipe exception")
             err = ev.errors[0]
-            print "Error desc:     ", err.desc
-            print "Error origin:   ", err.origin
-            print "Error reason:   ", err.reason
-            print "Error severity: ", err.severity
+            print("Error desc:     ", err.desc)
+            print("Error origin:   ", err.origin)
+            print("Error reason:   ", err.reason)
+            print("Error severity: ", err.severity)
         else:
             if ev.cmd_list is not None:
-                print "Number of commands   ", len(ev.cmd_list)
+                print("Number of commands   ", len(ev.cmd_list))
                 for i in range(len(ev.cmd_list)):
                     cmdInfo = ev.cmd_list[i]
-                    print "cmd  -----> ", cmdInfo.cmd_name
+                    print("cmd  -----> ", cmdInfo.cmd_name)
             else:
-                print "Number of commands    0"
+                print("Number of commands    0")
 
             if ev.att_list is not None:
-                print "Number of attributes ", len(ev.att_list)
+                print("Number of attributes ", len(ev.att_list))
                 for i in range(len(ev.att_list)):
                     attInfo = ev.att_list[i]
-                    print "att  -----> ", attInfo.name
+                    print("att  -----> ", attInfo.name)
             else:
-                print "Number of attributes  0"
+                print("Number of attributes  0")
 
-            print "Device started ", ev.dev_started
+            print("Device started ", ev.dev_started)
 
 def main():
     dev=tango.DeviceProxy('ifchangeServer/tango/1')
