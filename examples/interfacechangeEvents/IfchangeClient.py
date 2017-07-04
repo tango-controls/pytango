11c8c093d6c530add5523a1c90521b88eb2de8f6
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2017-07-04 10:29:02 +0100

    correct conflicts

--- examples/interfacechangeEvents/IfchangeClient.py
+++ examples/interfacechangeEvents/IfchangeClient.py
@@ -1,57 +1,60 @@
-import PyTango
-import numpy
+from __future__ import print_function
+
 import time
-from PyTango import DevState
+
+import tango
+
 
 class EventManager():
-     
+
     def __init__(self, dp):
         self._deviceProxy = dp
         if dp is not None:
-            print "Subscribed to Interface Change Events"
-            self._event_id = dp.subscribe_event(PyTango.EventType.INTERFACE_CHANGE_EVENT, self)
- 
+            print("Subscribed to Interface Change Events")
+            self._event_id = dp.subscribe_event(
+                tango.EventType.INTERFACE_CHANGE_EVENT, self)
+
     def unsubscribe(self):
         self._deviceProxy.unsubscribe_event(self._event_id)
- 
+
     def push_event(self, ev):
-        print "Event -----push_event-----------------"
-        print "Timestamp:      ", ev.reception_date
-        print "Event type:     ", ev.event
-        print "Device server:  ", ev.device
-        print "Event error:    ", ev.err
+        print("Event -----push_event-----------------")
+        print("Timestamp:      ", ev.reception_date)
+        print("Event type:     ", ev.event)
+        print("Device server:  ", ev.device)
+        print("Event error:    ", ev.err)
         if ev.err:
-            print "Caught pipe exception"
+            print("Caught pipe exception")
             err = ev.errors[0]
-            print "Error desc:     ", err.desc
-            print "Error origin:   ", err.origin
-            print "Error reason:   ", err.reason
-            print "Error severity: ", err.severity
+            print("Error desc:     ", err.desc)
+            print("Error origin:   ", err.origin)
+            print("Error reason:   ", err.reason)
+            print("Error severity: ", err.severity)
         else:
             if ev.cmd_list is not None:
-                print "Number of commands   ", len(ev.cmd_list);
+                print("Number of commands   ", len(ev.cmd_list))
                 for i in range(len(ev.cmd_list)):
                     cmdInfo = ev.cmd_list[i]
-                    print "cmd  -----> ", cmdInfo.cmd_name
+                    print("cmd  -----> ", cmdInfo.cmd_name)
             else:
-                print "Number of commands    0";
+                print("Number of commands    0")
 
             if ev.att_list is not None:
-                print "Number of attributes ", len(ev.att_list);
+                print("Number of attributes ", len(ev.att_list))
                 for i in range(len(ev.att_list)):
                     attInfo = ev.att_list[i]
-                    print "att  -----> ", attInfo.name
+                    print("att  -----> ", attInfo.name)
             else:
-                print "Number of attributes  0";
+                print("Number of attributes  0")
 
-            print "Device started ", ev.dev_started;
+            print("Device started ", ev.dev_started)
 
 
 def main():
-    dev=PyTango.DeviceProxy('ifchangeServer/tango/1')
-    print dev
-    em = EventManager(dev)
+    dev = tango.DeviceProxy('ifchangeServer/tango/1')
+    EventManager(dev)
     time.sleep(3000.0)
+
 
 if __name__ == '__main__':
     main()
--- examples/interfacechangeEvents/IfchangeServer.py
+++ examples/interfacechangeEvents/IfchangeServer.py
@@ -1,58 +1,45 @@
-import numpy
-import gevent
 import logging
-from numpy import void
 
-# PyTango imports
-import PyTango
-from PyTango import DebugIt
-from PyTango.server import run
-from PyTango.server import Device, DeviceMeta
-from PyTango.server import attribute, command
-from PyTango.server import class_property, device_property
-from PyTango import AttrQuality, AttrWriteType, DispLevel, DevState
+import tango
+from tango.server import Device
+from tango.server import DispLevel
+from tango.server import attribute, command
+
 
 class IfchangeServer(Device):
-
-    # ---------------
-    # General methods
-    # ---------------
-
-    def __init__(self, *args, **kwargs):
-        Device.__init__(self, *args, **kwargs)
 
     def init_device(self):
         Device.init_device(self)
         logging.basicConfig(level=logging.DEBUG)
         self._current = 0.0
-        self.set_state(PyTango.DevState.ON)
+        self.set_state(tango.DevState.ON)
 
     @attribute(label='Sequence Counter', dtype='int',
-        description="Sequence counter")
+               description="Sequence counter")
     def seq_counter(self):
         return 456
 
     @attribute(label='Voltage', dtype='float',
-        description="voltage")
+               description="voltage")
     def volts(self):
         return 3.142
 
     def read_current(self):
-        print "read current method ", self._current
+        print ("read current method ", self._current)
         return self._current
 
     def write_current(self, curr):
         self._current = curr
-        print "set current to ", self._current
-   
+        print ("set current to ", self._current)
+
     def start(self, argin):
-        print "start method"
+        print ("start method")
         return 3142
 
     @command(dtype_in=str, doc_in='name of dynamic attribute to add')
     def add_dyn_attr(self, name):
-        attr = attribute(name=name, dtype='float', 
-                          fget=self.read_current, fset=self.write_current)
+        attr = attribute(name=name, dtype='float',
+                         fget=self.read_current, fset=self.write_current)
         self.add_attribute(attr)
 
     @command(dtype_in=str, doc_in='name of dynamic attribute to delete')
@@ -62,24 +49,19 @@ class IfchangeServer(Device):
     @command
     def add_dyn_cmd(self):
         device_level = True
-        cmd = command(f=self.start, dtype_in=str, doc_in='description of input', dtype_out=int, doc_out='description of output',
+        cmd = command(f=self.start, dtype_in=str, dtype_out=int,
+                      doc_in='description of input',
+                      doc_out='description of output',
                       display_level=DispLevel.EXPERT, polling_period=5.1)
         self.add_command(cmd, device_level)
 
     @command(dtype_in=str, doc_in='name of dynamic command to delete')
     def delete_dyn_cmd(self, name):
         self._remove_command(name, False, True)
-        pass
+
 
 # ----------
 # Run server
 # ----------
-
-def main():
-    from PyTango import GreenMode
-    from PyTango.server import run
-
-    run([IfchangeServer,], green_mode=GreenMode.Gevent)
-
 if __name__ == '__main__':
-    main()
+    IfchangeServer.run_server()
