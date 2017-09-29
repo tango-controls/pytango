fa6caa9bd034199f130cd091dec53207eddfec48
NexeyaSGara <sebastien.gara@nexeya.com>
2017-09-29 10:18:11 +0200

    Fix version_long

--- tango/release.py
+++ tango/release.py
@@ -49,7 +49,7 @@ class Release:
     version = '.'.join(map(str, version_info[:3]))
     release = ''.join(map(str, version_info[3:]))
     separator = '.' if 'dev' in release or 'post' in release else ''
-    version_long = "9.2.5"
+    version_long = "9.2.3"
 
     version_description = 'This version implements the C++ Tango 9.2 API.'
     version_number = int(version.replace('.', ''))
