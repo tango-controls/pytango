a7587bc1d6716c35900be4212d15157b27eab4fe
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2018-03-12 16:13:42 +0000

    uncomment tests

--- testscript2.py
+++ testscript2.py
@@ -10,43 +10,43 @@ from tango import DbDatum
 from tango import Release
 dp = DeviceProxy('sys/tg_test/1')
 
-# # nx = 32
-# # spectre = np.array([float(value*value) for value in range(nx)])
-# # dp.write_attribute('double_spectrum', spectre)
-# # attr = dp.read_attribute('double_spectrum')
-# # assert attr.data_format == tango._tango.AttrDataFormat.SPECTRUM
-# # assert attr.dim_x == nx
-# # assert attr.dim_y == 0
-# # assert attr.has_failed == False
-# # assert attr.is_empty == False
-# # assert attr.name == 'double_spectrum'
-# # assert attr.nb_read == nx
-# # assert attr.nb_written == nx
-# # assert attr.quality == tango._tango.AttrQuality.ATTR_VALID
-# # assert type(attr.r_dimension) == tango._tango.AttributeDimension
-# # assert attr.r_dimension.dim_x == nx
-# # assert attr.r_dimension.dim_y == 0
-# # assert type(attr.time) == tango._tango.TimeVal
-# # assert attr.type == tango._tango.CmdArgType.DevDouble
-# # assert attr.value.all() == spectre.all()
-# # assert attr.w_dim_x == nx
-# # assert attr. w_dim_y == 0
-# # assert type(attr.w_dimension) == tango._tango.AttributeDimension
-# # assert attr.w_dimension.dim_x == nx
-# # assert attr.w_dimension.dim_y == 0
-# # assert attr.w_value.all() == spectre.all()
-# 
-# # spectre = np.array([True, True, False, True, False])
-# # dp.write_attribute('boolean_spectrum', spectre)
-# # attr = dp.read_attribute('boolean_spectrum')
-# # assert attr.value.all() == spectre.all()
-# # assert attr.w_value.all() == spectre.all()
-# 
-# # spectre = [i*i*i for i in range(32)]
-# # dp.write_attribute('long_spectrum', spectre)
-# # attr = dp.read_attribute('long_spectrum')
-# # assert attr.value.all() == np.array(spectre).all()
-# # assert attr.w_value.all() == np.array(spectre).all()
+nx = 32
+spectre = np.array([float(value*value) for value in range(nx)])
+dp.write_attribute('double_spectrum', spectre)
+attr = dp.read_attribute('double_spectrum')
+assert attr.data_format == tango._tango.AttrDataFormat.SPECTRUM
+assert attr.dim_x == nx
+assert attr.dim_y == 0
+assert attr.has_failed == False
+assert attr.is_empty == False
+assert attr.name == 'double_spectrum'
+assert attr.nb_read == nx
+assert attr.nb_written == nx
+assert attr.quality == tango._tango.AttrQuality.ATTR_VALID
+assert type(attr.r_dimension) == tango._tango.AttributeDimension
+assert attr.r_dimension.dim_x == nx
+assert attr.r_dimension.dim_y == 0
+assert type(attr.time) == tango._tango.TimeVal
+assert attr.type == tango._tango.CmdArgType.DevDouble
+assert attr.value.all() == spectre.all()
+assert attr.w_dim_x == nx
+assert attr. w_dim_y == 0
+assert type(attr.w_dimension) == tango._tango.AttributeDimension
+assert attr.w_dimension.dim_x == nx
+assert attr.w_dimension.dim_y == 0
+assert attr.w_value.all() == spectre.all()
+
+spectre = np.array([True, True, False, True, False])
+dp.write_attribute('boolean_spectrum', spectre)
+attr = dp.read_attribute('boolean_spectrum')
+assert attr.value.all() == spectre.all()
+assert attr.w_value.all() == spectre.all()
+
+spectre = [i*i*i for i in range(32)]
+dp.write_attribute('long_spectrum', spectre)
+attr = dp.read_attribute('long_spectrum')
+assert attr.value.all() == np.array(spectre).all()
+assert attr.w_value.all() == np.array(spectre).all()
 
 spectre = np.array(['ESRF', 'ALBA', 'MAXIV', 'PETRA3', 'SOLEIL', 'SOLARIS'])
 dp.write_attribute('string_spectrum', spectre)
