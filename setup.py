f377df7c63856abda2ecf3ce9a8360f168b4a8d5
Vincent Michel <vincent.michel@esrf.fr>
2018-05-29 18:37:58 +0200

    Add python 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -466,6 +466,7 @@ def setup_args():
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development :: Libraries',
     ]
