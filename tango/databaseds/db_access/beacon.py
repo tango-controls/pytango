c2b79cc167a0357d7a4ed9dd907d87c4215fc40b
Matias Guijarro <guijarro@esrf.fr>
2018-11-06 12:08:58 +0100

    use special case-insensitive weak values dictionary for Tango nodes indexing
    Fixes the 'put_device_alias' bug, and probably many more we did not see yet

--- tango/databaseds/db_access/beacon.py
+++ tango/databaseds/db_access/beacon.py
@@ -38,6 +38,38 @@ def _filter(wildcard,l) :
     m = re.compile(wildcard,re.IGNORECASE)
     return [x for x in l if x is not None and m.match(x)]
 
+class CaseInsensitiveDict(weakref.WeakValueDictionary, object):
+    @classmethod
+    def _k(cls, key):
+        return key.lower() if isinstance(key, basestring) else key
+
+    def __init__(self, *args, **kwargs):
+        super(CaseInsensitiveDict, self).__init__(*args, **kwargs)
+        self._convert_keys()
+    def __getitem__(self, key):
+        return super(CaseInsensitiveDict, self).__getitem__(self.__class__._k(key))
+    def __setitem__(self, key, value):
+        super(CaseInsensitiveDict, self).__setitem__(self.__class__._k(key), value)
+    def __delitem__(self, key):
+        return super(CaseInsensitiveDict, self).__delitem__(self.__class__._k(key))
+    def __contains__(self, key):
+        return super(CaseInsensitiveDict, self).__contains__(self.__class__._k(key))
+    def has_key(self, key):
+        return super(CaseInsensitiveDict, self).has_key(self.__class__._k(key))
+    def pop(self, key, *args, **kwargs):
+        return super(CaseInsensitiveDict, self).pop(self.__class__._k(key), *args, **kwargs)
+    def get(self, key, *args, **kwargs):
+        return super(CaseInsensitiveDict, self).get(self.__class__._k(key), *args, **kwargs)
+    def setdefault(self, key, *args, **kwargs):
+        return super(CaseInsensitiveDict, self).setdefault(self.__class__._k(key), *args, **kwargs)
+    def update(self, E={}, **F):
+        super(CaseInsensitiveDict, self).update(self.__class__(E))
+        super(CaseInsensitiveDict, self).update(self.__class__(**F))
+    def _convert_keys(self):
+        for k in list(self.keys()):
+            v = super(CaseInsensitiveDict, self).pop(k)
+            self.__setitem__(k, v)
+
 class beacon(object):
 
     DB_API_NAME = 'beacon'
@@ -73,9 +105,9 @@ class beacon(object):
     def _index(self):
         #Tango indexing
         self._strong_node_ref = set()
-        self._personal_2_node = weakref.WeakValueDictionary()
-        self._tango_name_2_node = weakref.WeakValueDictionary()
-        self._class_name_2_node = weakref.WeakValueDictionary()
+        self._personal_2_node = CaseInsensitiveDict()
+        self._tango_name_2_node = CaseInsensitiveDict()
+        self._class_name_2_node = CaseInsensitiveDict()
 
         for key,values in self._config.root.iteritems():
             indexing_flag = key == 'tango'
