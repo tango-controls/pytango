4aa4764df9a601ffcc3a6deaf80e50d71653148e
Geoffrey Mant <geoffrey.mant@stfc.ac.uk>
2019-11-26 14:10:40 +0000

    remove old versions

--- testscript11.py
+++
@@ -1,9 +0,0 @@
-import tango
-import gc
-while True:
-    gc.collect()
-    print(len(gc.get_objects()))
-    prox=tango.DeviceProxy('sys/tg_test/1')
-    print(repr(prox))
-    prox.write_attribute = prox.write_attribute
-    print(prox.get_fqdn())
