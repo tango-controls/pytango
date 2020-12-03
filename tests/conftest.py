25012e96114e46c3dfb6f57f04a3fa0a857c5668
spirit1317 <matcelary@gmail.com>
2020-12-03 08:52:24 +0100

    windows tests fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,23 @@
 
 from tango.test_utils import state, typed_values, server_green_mode
 
+import pytest
+
+import sys
+import os
+import json
+
 __all__ = ('state', 'typed_values', 'server_green_mode')
+
+@pytest.hookimpl()
+def pytest_sessionfinish(session):
+    """ collets all tests to be run and outputs to bat script """
+    if '--collect-only' in sys.argv and '-q' in sys.argv and 'nt' in os.name:
+        print("Generating windows test script...")
+        script_path = os.path.join(os.path.dirname(__file__),'run_tests_win.bat')
+        with open(script_path,"w") as f:
+            f.write("REM this script will run all tests separately.")
+            for item in session.items:
+                f.write("\n")
+                f.write("pytest -c ../pytest_empty_config.txt ")#this empty file is created by appveyor
+                f.write(item.nodeid) 
